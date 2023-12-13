4ce941ce8a581ab51c6e5ffc00412894bb387eed
Ubuntu <ubuntu@ip-172-31-91-186.ec2.internal>
2023-12-13 13:33:40 +0000

    Todo

--- scripts/deploy_moodle.sh
+++ scripts/deploy_moodle.sh
@@ -24,6 +24,9 @@ apt install unzip -y
 # Descargar los archivos de dentro del zip descargado anteriormente
 unzip /var/www/html/v4.3.1.zip -d /var/www/html/
 
+# Mover al directorio raíz
+mv /var/www/html/moodle-4.3.1/* /var/www/html
+
 # Configuramos el parametro max_input_vars en 5000
 sed -i "s/;max_input_vars = 1000/max_input_vars = $max_input_vars/" /etc/php/8.1/apache2/php.ini
 sed -i "s/;max_input_vars = 1000/max_input_vars = $max_input_vars/" /etc/php/8.1/cli/php.ini
@@ -50,9 +53,9 @@ sudo mkdir /var/www/moodledata
 chmod 077 /var/www/moodledata
 
 # Borramos instalaciones previas 
-rm -rf /var/www/html/admin/moodle-4.3.1/install.php
+rm -rf /var/www/html/moodle-4.3.1/install.php
 # Instalamos moodle con cli
-sudo -u www-data php /var/www/html/moodle-4.3.1/install.php \
+sudo -u www-data php /var/www/html/admin/cli/install.php \
     --lang=$MOODLE_LANG \
     --wwwroot=$MOODLE_WWWROOT \
     --dataroot=$MOODLE_DATAROOT \
@@ -69,6 +72,8 @@ sudo -u www-data php /var/www/html/moodle-4.3.1/install.php \
     -adminemail=$MOODLE_ADMIN_EMAIL \
     --non-interactive \
     --agree-license
+
+
 
 # Reiniciamos el servicio de apache
 systemctl restart apache2
