bd62c16184727f91df8b9c4c3a00ce2c2d680e28
Ubuntu <ubuntu@ip-172-31-83-82.ec2.internal>
2023-12-15 13:02:10 +0000

    todo

--- scripts/deploy_frontend_phpmyadmin.sh
+++ scripts/deploy_frontend_phpmyadmin.sh
@@ -9,49 +9,21 @@ source .env
 # Actualizamos los paquetes
 apt update -y
 
-#Instalamos los modulos de php necesarios para phpMyAdmin.
-sudo apt install php-mbstring php-zip php-json php-gd php-fpm php-xml -y
+# Instalamos Adminer 
 
-#Despues de la inctalacion es necesario reinicoar el servicio de apache
-systemctl restart apache2
+# Creamos el directorio para adminer 
+mkdir -p /var/www/html/adminer
 
 # Instalamos la utilidad wget
 apt install wget -y 
 
-#Eliminamos descargas previas de phpMyAdmin
-rm -rf /tmp/phpMyAdmin-5.2.1-all-languages.zip
+wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php -P /var/www/html/adminer
 
-# Eliminamos instalaciones previad de phpMyAdmin
-rm -rf /var/www/html/phpmyadmin
+# Renombramos el nombre del archivo de Adminer 
+mv /var/www/html/adminer/adminer-4.8.1-mysql.php /var/www/html/adminer/index.php
 
-# Descargamos el codigo fuente de phpMyadmin
-wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.zip -P /tmp
-
-#Instalamos la utilidad unzip 
-apt install unzip -y
-
-# Descomprimimos el codigo fuente de phpMyAdmin en /var/www/html
-unzip -u /tmp/phpMyAdmin-5.2.1-all-languages.zip -d /var/www/html
-
-# Renombramos el directorio de phpMyAdmin
-mv /var/www/html/phpMyAdmin-5.2.1-all-languages /var/www/html/phpmyadmin
-
-# Actualizamos los permisos del directorio /var/www/html
+# Modificamos el propietario y el grupo del directorio /var/www/html
 chown -R www-data:www-data /var/www/html
 
-# Creamos un archivo de configuracion a partir del archivo e ejemplo
-cp /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
-
-# Generamos la variable blowfish_secret 
-RANDOM_VALUE=`openssl rand -hex 16`
-
-# Modificamos la variable blowfish_secret
-sed -i "s/\(\$cfg\['blowfish_secret'\] =\).*/\1 '$RANDOM_VALUE';/" /var/www/html/phpmyadmin/config.inc.php
-
-#creamos un directorio temporal para phpmyadmin
-mkdir -p /var/www/html/phpmyadmin/tmp/
-
-# Actualizamos los permisos del directorio /var/www/html
-chown -R www-data:www-data /var/www/html
 
 
